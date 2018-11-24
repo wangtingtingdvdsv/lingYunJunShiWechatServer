fe293e90aab55cd91b7daf65b53ce5044e316f29
wangtingting <2372454518@qq.com>
2018-11-24 20:17:49 +0800

    持续更新中

--- bin/lingYunJunShiWechatServer.js
+++ bin/lingYunJunShiWechatServer.js
@@ -1,4 +1,4 @@
-
+﻿
 
 
 /**
@@ -20,7 +20,7 @@ var path = require('path');
 
 
 
-var port = normalizePort(process.env.PORT || '9006');
+var port = normalizePort(process.env.PORT || '3006');
 // app.set('port', port);
 
 /**
@@ -34,7 +34,7 @@ var server = http.createServer(app.callback());
  */
 
 server.listen(port, function() {
-  console.log('9006端口启动成功');
+  console.log('3006端口启动成功');
 });
 server.on('error', onError);
 server.on('listening', onListening);
