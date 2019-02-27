cbbc83f102bfbc793cf29c395201adce90881c12
王婷婷 <2372454518@qq.com>
2019-02-27 19:53:47 +0800

    update

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ var config = {
 }
 const wechatApp = {
     appId : 'wxdfb7470ba6c115be',
-    secret : '92a4355dd2c56fb4f6007f38ce61348f',
+    secret : 'd9da1c81374d04cda8b56b17a5c42186 ',
     key : '7cb7b2b5c278af1ccfc3309aebcd8eec',
-    mch_id : '1522759081'
+    mch_id : '1525987971'
 }
 module.exports={
     config,
