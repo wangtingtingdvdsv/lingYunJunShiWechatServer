9f4d6a915338ac2d2654a7bcc03ce0234fd833bd
王婷婷 <2372454518@qq.com>
2019-02-27 20:04:14 +0800

    update

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const wechatApp = {
     appId : 'wxdfb7470ba6c115be',
     secret : '92a4355dd2c56fb4f6007f38ce61348f',
     key : 'd9da1c81374d04cda8b56b17a5c42186',
-    mch_id : '1525987971 '
+    mch_id : '1525987971'
 }
 module.exports={
     config,
