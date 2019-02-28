81b248f9a6282cae93e953d26b59b8e4eaa5af0d
wangtingting <2372454518@qq.com>
2019-02-28 23:36:45 +0800

    update

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ var config = {
 }
 const wechatApp = {
     appId : 'wxdfb7470ba6c115be',
-    secret : '92a4355dd2c56fb4f6007f38ce61348f',
-    key : 'd9da1c81374d04cda8b56b17a5c42100',
+    secret : 'bb1c52b621a7d16fbbad7b0a9f651dcb',
+       key : 'd9da1c81374d04cda8b56b17a5c42100',
     mch_id : '1525987971'
 }
 module.exports={
